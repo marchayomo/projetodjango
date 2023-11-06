fe6ae77d143a01f950aab0bd2564451b4ddf287b
marchayomo <marcelo.hayami@gmail.com>
2023-11-06 16:38:04 -0300

    Models e admin

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -27,3 +29,5 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+    def __str__(self):
+        return self.title
