231a02e50c503dddd6e1e0f25b8774dbac1a1801
marchayomo <marcelo.hayami@gmail.com>
2023-11-07 10:44:09 -0300

    pagina not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    return render(request, 'recipes/pages/home.html', context= {
+    return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
@@ -17,8 +18,17 @@ def category(request, category_id):
         is_published=True,
     ).order_by('-id')
 
+    if not recipes:
+        raise Http404('Not found')
+    
+    # Uma forma alternativa envolvendo short cuts
+    # recipes= get_list_or_404 (Recipe.objects.filter(
+    #    category__id=category_id,
+    #    is_published=True,).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context= {
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 
